b58461c1ad964e66011f2cfd4573a71941661dc8
Toby Petty <tobypetty1@gmail.com>
2022-01-27 17:01:03 +0000

    Added collection test

--- tests/integration/test_base.py
+++ tests/integration/test_base.py
@@ -7,14 +7,16 @@ from unittest.mock import Mock
 
 import pytest
 
-from dags.base import callable_collect_task
+from dags.base import callable_collect_task, callable_collection_task
 
+
+TODAY = date.today()
 
 @pytest.fixture
-def collection_metadata_dir(tmpdir):
-    collection_dir = tmpdir.join("collection")
+def collection_metadata_dir(tmp_path):
+    collection_dir = tmp_path.joinpath("collection")
     copytree(
-        Path(__file__).parent.parent.joinpath("data/collection"),
+        Path(__file__).parent.parent.joinpath("data/collection/csv"),
         collection_dir,
         dirs_exist_ok=True,
     )
@@ -22,35 +24,78 @@ def collection_metadata_dir(tmpdir):
 
 
 @pytest.fixture
+def collection_payload_dir(tmp_path):
+    log_dir = (
+        tmp_path.joinpath("collection").joinpath("log").joinpath(TODAY.isoformat())
+    )
+    copytree(
+        Path(__file__).parent.parent.joinpath("data/collection/log"),
+        log_dir,
+        dirs_exist_ok=True,
+    )
+    return log_dir
+
+
+@pytest.fixture
 def endpoint_requests_mock(requests_mock, collection_metadata_dir):
-    with open(collection_metadata_dir.join("endpoint.csv")) as f:
+    with open(collection_metadata_dir.joinpath("endpoint.csv")) as f:
         endpoint_contents = DictReader(f)
         return [
-            requests_mock.get(row["endpoint-url"])
+            requests_mock.get(
+                row["endpoint-url"], json={"iamaresponsefrom": row["endpoint-url"]}
+            )
             for row in endpoint_contents
             # Implicit test of this logic in the code as requests mock will throw
             # requests_mock.exceptions.NoMockAddress if un-mocked URL requested
-            if not row["end-date"]
-            or date.fromisoformat(row["end-date"]) >= date.today()
+            if not row["end-date"] or date.fromisoformat(row["end-date"]) >= TODAY
         ]
 
 
-def test_collect(endpoint_requests_mock, tmpdir, collection_metadata_dir):
-    # Setup
-    tmpdir.mkdir("pipeline")
-    kwargs = {
-        "ti": Mock(**{"xcom_pull.return_value": tmpdir}),
+@pytest.fixture
+def kwargs(tmp_path):
+    return {
+        "ti": Mock(**{"xcom_pull.return_value": tmp_path}),
         "dag": Mock(**{"_dag_id": "listed-building"}),
     }
 
+
+def test_collect(collection_metadata_dir, endpoint_requests_mock, kwargs, tmp_path):
+    # Setup
+    tmp_path.joinpath("pipeline").mkdir()
+
     # Call
-    response = callable_collect_task(**kwargs)
+    callable_collect_task(**kwargs)
 
     # Assert
-    log_dir = collection_metadata_dir.join("log").join(date.today().isoformat())
+    log_dir = collection_metadata_dir.joinpath("log").joinpath(TODAY.isoformat())
     all_urls = {endpoint._url for endpoint in endpoint_requests_mock}
-    all_logs = [load(f.open()) for f in log_dir.listdir()]
+    all_logs = [load(f.open()) for f in log_dir.iterdir()]
     assert {log["endpoint-url"] for log in all_logs} == all_urls
     for mock in endpoint_requests_mock:
         assert mock.called_once
         assert mock._url in [log["endpoint-url"] for log in all_logs]
+
+
+def test_collection(collection_metadata_dir, collection_payload_dir, kwargs, tmp_path):
+    # Setup
+    tmp_path.joinpath("pipeline").mkdir()
+
+    # Call
+    callable_collection_task(**kwargs)
+
+    # Assert
+    with open(collection_payload_dir.parent.parent.joinpath("log.csv")) as log_file:
+        log_csv = DictReader(log_file)
+        logs = list(log_csv)
+        assert {log["endpoint"] for log in logs} == set(
+            path.name[: -len("".join(path.suffixes))]
+            for path in collection_payload_dir.iterdir()
+        )
+
+        with open(
+            collection_payload_dir.parent.parent.joinpath("resource.csv")
+        ) as resource_file:
+            resource_csv = DictReader(resource_file)
+            assert {log["resource"] for log in logs} == {
+                resource["resource"] for resource in resource_csv
+            }
