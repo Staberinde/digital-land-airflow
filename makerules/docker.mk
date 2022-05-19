ec0d47a8658e1363a8ff4e62902ff98b65273369
Toby Petty <tobypetty1@gmail.com>
2022-05-19 17:01:41 +0100

    Also build and push image as latest (so terraform apply doesn't accidentily revert helm deploy)

--- makerules/docker.mk
+++ makerules/docker.mk
@@ -3,17 +3,20 @@ include makerules/makerules.mk
 # TODO add this ECR repository to terraform
 BUILD_REPO := public.ecr.aws/l6z6v3j6
 GIT_COMMIT := $(shell git show -s --format=%H)
-BUILD_TAG := $(BUILD_REPO)/digital-land-airflow:$(GIT_COMMIT)
+BUILD_IMAGE:= $(BUILD_REPO)/digital-land-airflow
+BUILD_TAG := $(BUILD_IMAGE):$(GIT_COMMIT)
 
 # We are baking resources (to be declarative) into the image so we don't want them cached
 docker-build: docker-check
 	docker build -t $(BUILD_TAG) . --no-cache
+	docker tag $(BUILD_TAG) $(BUILD_IMAGE):latest
 
 docker-login:
 	aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
 
 docker-push: docker-check docker-login
 	docker push $(BUILD_TAG)
+	docker push $(BUILD_IMAGE):latest
 
 docker-test: docker-check
 	docker run -t --user airflow --entrypoint bash $(BUILD_TAG) -c 'pytest $$(python -c "import inspect, os; from digital_land_airflow import tests; print(os.path.dirname(inspect.getfile(tests)))") -p digital_land_airflow.tests.fixtures.base'
