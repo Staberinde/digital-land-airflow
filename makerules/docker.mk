96c2382900133ba5f25a2db66f19b5c4e549c6fa
Toby Petty <tobypetty1@gmail.com>
2022-08-10 11:46:39 +0100

    Add guard against trying to deploy an image that does not exist in ECR

--- makerules/docker.mk
+++ makerules/docker.mk
@@ -25,22 +25,25 @@ docker-test: docker-check
 	docker run -t --user airflow --entrypoint bash $(BUILD_TAG) -c 'pytest $$(python -c "import inspect, os; from digital_land_airflow import tests; print(os.path.dirname(inspect.getfile(tests)))") -p digital_land_airflow.tests.fixtures.base'
 
 docker-check:
-ifeq (, $(shell which docker))
+ifeq (,$(shell which docker))
 	$(error "No docker in $(PATH), consider doing apt-get install docker OR brew install --cask docker")
 endif
 
 helm-check:
-ifeq (, $(shell which helm))
+ifeq (,$(shell which helm))
 	$(error "No helm in $(PATH), consider following apt install instructions https://helm.sh/docs/intro/install/#from-apt-debianubuntu OR snap install --classic helm OR brew install --cask helm")
 endif
-ifeq (, $(ENVIRONMENT))
+ifeq (,$(ENVIRONMENT))
 	$(error "No environment specified via $$ENVIRONMENT, please pass as make argument")
 endif
 
 helm-login: helm-check
-	aws eks update-kubeconfig --region eu-west-2 --name $(ENVIRONMENT)-planning-data
+	# aws eks update-kubeconfig --region eu-west-2 --name $(ENVIRONMENT)-planning-data
 
 helm-deploy: helm-login
+ifeq (,$(shell docker manifest inspect $(BUILD_TAG)))
+	$(error "Image for commit $(BUILD_TAG) does not exist in ECR registry. Please ensure commit is in upstream main, and that no tests have failed in `Build and deploy airflow DAG docker image` Github Action before attempting deploy")
+endif
 	helm repo add apache-airflow https://airflow.apache.org/
 	helm repo update
 	helm upgrade airflow-stable apache-airflow/airflow --namespace $(ENVIRONMENT)-pipelines --reuse-values --set images.airflow.tag=$(GIT_COMMIT) --kubeconfig ~/.kube/config
